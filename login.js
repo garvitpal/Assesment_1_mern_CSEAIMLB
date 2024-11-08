a9eb96a18cf3c7564c896cee5514a623863d4b70
garvitpal <159601535+garvitpal@users.noreply.github.com>
2024-11-08 10:28:45 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+function login() {
+    var userId = document.getElementById('userId').value;
+    var password = document.getElementById('password').value;
+
+    if (userId === '' || password === '') {
+        alert('User ID and Password cannot be empty');
+        return;
+    }
+
+    if (userId !== 'admin' || password !== 'admin') {
+        alert('Invalid User ID or Password');
+        return;
+    }
+
+    window.location.href ='index.html';
+}
